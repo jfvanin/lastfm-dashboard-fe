00b5e4c73ff28051c76b20ef59f9fa4d1198c733
Jose Vanin <jose@shipamax.com>
2025-05-13 23:33:43 +0700

    Add Full Screen button

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-  import { faXmark } from '@fortawesome/free-solid-svg-icons';
+  import { faXmark, faExpand, faCompress } from '@fortawesome/free-solid-svg-icons';
   import { faGithub } from '@fortawesome/free-brands-svg-icons';
   import { defineProps, defineEmits, ref, watch } from 'vue';
 
@@ -10,11 +10,30 @@
 
   const userInput = ref('');
   const loading = ref(false);
+  const isFullscreen = ref(false);
 
   const emit = defineEmits<{
     (event: 'setUser', user: string): void,
     (event: 'clearUser'): void
   }>();
+
+  const toggleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen().then(() => {
+        isFullscreen.value = true;
+      }).catch(err => {
+        console.error(`Error attempting to enable fullscreen: ${err.message}`);
+      });
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen().then(() => {
+          isFullscreen.value = false;
+        }).catch(err => {
+          console.error(`Error attempting to exit fullscreen: ${err.message}`);
+        });
+      }
+    }
+  };
 
   const updateUser = () => {
     if (userInput.value) {
@@ -38,7 +57,11 @@
     <span class="wrapper flex-1 font-sans text-xl ml-3">
       <strong>LastFm</strong> Mene Dashboard
     </span>
-    <div class="float-right mr-3 mt-1">
+    <div class="float-right mr-3 mt-1 flex items-center gap-4">
+      <button @click="toggleFullscreen" class="text-white"
+        :title="isFullscreen ? 'Exit Fullscreen' : 'Enter Fullscreen'">
+        <FontAwesomeIcon :icon="isFullscreen ? faCompress : faExpand" class="text-xl" />
+      </button>
       <a href="https://github.com/jfvanin/lastfm-dashboard-fe" target="_blank" class="text-white" alt="GitHub"
         title="GitHub">
         <FontAwesomeIcon :icon="faGithub" class="text-2xl" />
